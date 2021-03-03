977cd3bd272b64dfb56f446dc8d895f5872146e5
Eduardo Moraes <edumoraes@live.com>
2021-03-03 15:39:46 -0300

    test

---
+++ test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  3 15:20:35 2021
+
+@author: Eduardo
+"""
+
+print('hello world')
+print("hello world 2")
+print(6*7)
+print(6/0)
+print('this is the end part')
